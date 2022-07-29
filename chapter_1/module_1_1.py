ce8a5aae7037b7fb98d9ef365191b542869de6ed
mirzasaad <saaad.mirxa@gmail.com>
2022-07-29 08:04:15 +0500

    bst

--- chapter_1/module_1_1.py
+++ chapter_1/module_1_1.py
@@ -1,5 +1,8 @@
 
 
+from unicodedata import numeric
+
+
 def gcd(p, q):
     '''
     Calculate greatest common divisor of two numbers.
@@ -36,11 +39,75 @@ def is_prime(number):
         return False
     i = 2
     while i * i <= number:
-        print(i)
         if (number % i == 0):
             return False
         i += 1
     
     return True
 
-print(is_prime(23))
+def sqrt(number):
+    '''
+    Calculate the square of the number(Newton's method).
+    >>> sqrt(4)
+    2.0
+    >>> sqrt(9)
+    3.0
+    >>> sqrt(1)
+    1
+    >>> sqrt(256)
+    16.0
+    '''
+
+    if (number == 0 or number == 1):
+        return number
+
+    i, result = 1, 1
+    while (result <= number):
+        i += 1
+        result = i * i
+    
+    return i - 1
+
+def harmonic(number):
+    '''
+    Calculate the harmonic number of the given number.
+    >>> harmonic(2)
+    1.5
+    >>> harmonic(3)
+    1.8333333333333333
+    '''
+    return sum([1 / i for i in range(1, number + 1)])
+
+
+def binary_search(key, lst):
+    '''
+    Determine whether the key in target list.
+    Return the index of the key in the given ascending list(i - 1),
+    if the key not in the list, return -1.
+    >>> binary_search(3, [1, 2, 3, 4, 5])
+    2
+    >>> binary_search(1, [1, 2, 3, 4, 5, 6, 7, 9])
+    0
+    >>> binary_search(9, [1, 2, 3, 4, 5, 6, 7, 9])
+    7
+    >>> binary_search(999, [1, 2, 3, 4, 5, 6, 7, 9])
+    -1
+    '''
+
+    assert isinstance(key, int)
+    assert isinstance(lst, (list, tuple))
+
+    low, high = 0, len(lst) - 1
+
+    while low <= high:
+        mid = int((high + low) / 2)
+        if (lst[mid] == key):
+            return mid
+        elif (key < lst[mid]):
+            high = mid - 1
+        elif (key > lst[mid]):
+            low = mid + 1
+
+    return -1
+
+
