426d65479aebba857fe160a36962455f81a30c30
mirzasaad <saaad.mirxa@gmail.com>
2022-09-12 15:56:53 +0500

    Update interview_question_4_1.py

--- chapter_4/interview_question_4_1.py
+++ chapter_4/interview_question_4_1.py
@@ -1,10 +1,13 @@
 
-from collections import defaultdict
+from collections import defaultdict, deque
 import collections
+import doctest
 from enum import Enum
 from operator import itemgetter, ne
 import random
-from module_4_1 import Graph, BreadthFirstPaths, ConnectedComponent
+from basic_data_struct import Stack, Queue
+from module_4_1 import Graph, BreadthFirstPaths, ConnectedComponent, DepthFirstPaths, Cycle
+
 
 class DFSIterative():
     """
@@ -14,34 +17,66 @@ class DFSIterative():
     >>> test_data = [(0, 5), (4, 3), (0, 1), (9, 12), (6, 4), (5, 4), (0, 2), (11, 12), (9, 10), (0, 6), (7, 8), (9, 11), (5, 3)]
     >>> for a, b in test_data:
     ...     G.add_edge(a, b)
+    >>> dfs = DFSIterative(G)
+    >>> [i for i in dfs.dfsIterative()]
+    [0, 1, 2, 5, 3, 4, 6, 7, 8, 9, 10, 11, 12]
+    >>> [i for i in dfs.iterateRecursive()]
+    [0, 1, 2, 5, 3, 4, 6, 7, 8, 9, 10, 11, 12]
+    >>> [i for i in dfs.dfsIterative()] == [i for i in dfs.iterateRecursive()]
+    True
+    >>> assert [i for i in dfs.dfsIterative()] == [i for i in dfs.iterateRecursive()]
     """
 
     def __init__(self, g: Graph) -> None:
+        self._graph = g
+
+    def dfsIterative(self):
         self._marked = defaultdict(bool)
-        self._edgeTo = defaultdict(list)
+        result = []
 
-    def iterate(self):
-
-        for vertex in g.vertices():
+        for vertex in sorted(self._graph.vertices()):
             if not self._marked[vertex]:
-                self.dfs(vertex)
+                self.iterate(self._graph, vertex, result)
 
-    def dfs(self, graph: Graph, vertex):
-        self.marked[vertex] = True
+        return result
 
-        stack = [vertex]
+    def iterate(self, graph: Graph, vertex, result):
 
-        while stack:
-            current = stack[-1]
+        stack = Stack()
+        stack.push(vertex)
 
-            for neighbour in graph.get_adjacent_vertices(vertex):
-                if not self._marked[neighbour]:
-                    self._marked[neighbour] = True
-                    self._edgeTo[neighbour] = vertex
-                    yield current
-                    stack.append(neighbour)
-
+        while not stack.is_empty():
+            current = stack.peek()
             stack.pop()
+
+            if self._marked[current]:
+                continue
+
+            self._marked[current] = True
+            result.append(current)
+
+            for neighbour in sorted(graph.get_adjacent_vertices(current), reverse=True):
+                if not self._marked[neighbour]:
+                    stack.push(neighbour)
+
+    def iterateRecursive(self):
+        self._marked = defaultdict(bool)
+
+        result = Queue()
+
+        for vertex in sorted(self._graph.vertices()):
+            if not self._marked[vertex]:
+                self.dfs(self._graph, vertex, result)
+
+        return result
+
+    def dfs(self, graph: Graph, vertex, result):
+        self._marked[vertex] = True
+        result.enqueue(vertex)
+
+        for neighbour in sorted(graph.get_adjacent_vertices(vertex)):
+            if not self._marked[neighbour]:
+                self.dfs(graph, neighbour, result)
 
 
 class GraphProperties(object):
@@ -90,8 +125,8 @@ class GraphProperties(object):
 
 class EulerianType(Enum):
     NOT_A_EULER_GRAPH = 0
-    EULER_PATH = 0
-    EULER_CIRCUIT = 0
+    EULER_PATH = 1
+    EULER_CIRCUIT_OR_CYCLE = 2
 
 # https://algs4.cs.princeton.edu/code/edu/princeton/cs/algs4/EulerianCycle.java.html
 
@@ -126,14 +161,46 @@ class Eulerian(object):
 
     A Graph must have nodes with even degree and odd degree. All the odd degree nodes are either start or end but all the even degree node will be only intermediate nodes.
     But in case when all the nodes has even degree, then it contains the Eulerian Tour, as we we'll start from some node and will end to this same node, as we need to consume all the edges.
+    
+    
+    
+    >>> G = Graph()
+    >>> test_data = [(1, 0), (0, 2), (2, 1), (0, 3), (3, 4), (4, 0)]
+    >>> for a, b in test_data:
+    ...     G.add_edge(a, b)
+    >>> euler = Eulerian(G)
+    >>> '[    (isEuler ===>, {}),     (isConnected ===>, {}),     (hasCycle ===>, {})     ]'.format(euler.isEuler(), euler._cc.count() == 1, euler._cycle.has_cycle())
+    '[    (isEuler ===>, EulerianType.EULER_CIRCUIT_OR_CYCLE),     (isConnected ===>, True),     (hasCycle ===>, True)     ]'
+    >>> G = Graph()
+    >>> test_data = [(1, 0), (0, 2), (2, 1), (0, 3), (3, 4)]
+    >>> for a, b in test_data:
+    ...     G.add_edge(a, b)
+    >>> euler = Eulerian(G)
+    >>> '[    (isEuler ===>, {}),     (isConnected ===>, {}),     (hasCycle ===>, {})     ]'.format(euler.isEuler(), euler._cc.count() == 1, euler._cycle.has_cycle())
+    '[    (isEuler ===>, EulerianType.EULER_PATH),     (isConnected ===>, True),     (hasCycle ===>, True)     ]'
+    >>> G = Graph()
+    >>> test_data = [(1, 0), (0, 2), (2, 1), (0, 3), (3, 4), (1, 3)]
+    >>> for a, b in test_data:
+    ...     G.add_edge(a, b)
+    >>> euler = Eulerian(G)
+    >>> '[    (isEuler ===>, {}),     (isConnected ===>, {}),     (hasCycle ===>, {})     ]'.format(euler.isEuler(), euler._cc.count() == 1, euler._cycle.has_cycle())
+    '[    (isEuler ===>, EulerianType.NOT_A_EULER_GRAPH),     (isConnected ===>, True),     (hasCycle ===>, True)     ]'
+    >>> G = Graph()
+    >>> test_data = [(0, 1), (1, 2), (2, 0)]
+    >>> for a, b in test_data:
+    ...     G.add_edge(a, b)
+    >>> euler = Eulerian(G)
+    >>> '[    (isEuler ===>, {}),     (isConnected ===>, {}),     (hasCycle ===>, {})     ]'.format(euler.isEuler(), euler._cc.count() == 1, euler._cycle.has_cycle())
+    '[    (isEuler ===>, EulerianType.EULER_CIRCUIT_OR_CYCLE),     (isConnected ===>, True),     (hasCycle ===>, True)     ]'
     """
 
     def __init__(self, g: Graph) -> None:
         self._cc = ConnectedComponent(g)
+        self._cycle = Cycle(g)
         self._graph = g
 
     def isEuler(self):
-        if self._cc.count > 0:
+        if self._cc.count() > 1 or not self._cycle.has_cycle():
             return EulerianType.NOT_A_EULER_GRAPH
 
         number_of_odd_vertex_degree = self.odd_vertices()
@@ -141,23 +208,22 @@ class Eulerian(object):
         if number_of_odd_vertex_degree > 2:  # only start and end node can have odd degree, others  verteces should have even degrees
             return EulerianType.NOT_A_EULER_GRAPH
 
-        return EulerianType.EULER_CIRCUIT if number_of_odd_vertex_degree == 0 else EulerianType.EULER_PATH
+        return EulerianType.EULER_CIRCUIT_OR_CYCLE if number_of_odd_vertex_degree == 0 else EulerianType.EULER_PATH
 
     def odd_vertices(self):
         number_of_odd__vertex_degree = 0
 
         for vertex in self._graph.vertices():
-            if len(self._graph.degree(vertex)) & 1:
+            if self._graph.degree(vertex) & 1:
                 number_of_odd__vertex_degree += 1
 
         return number_of_odd__vertex_degree
 
     def get_random_vertex_with_odd_degree(self):
-        return random.sample([v for v in self._graph.vertices() if self._graph.degree(v) & 1], 1)
+        return random.sample([v for v in self._graph.vertices() if self._graph.degree(v) & 1], 1)[-1]
 
     def get_random_vertex(self):
-        return random.sample(self._graph.vertices(), 1)
-
+        return random.sample(self._graph.vertices(), 1)[-1]
 
     def trail(self):
         isEuler = self.isEuler()
@@ -176,23 +242,23 @@ class Eulerian(object):
         # Stop when you run out of edges.
 
         startVertex = self.get_random_vertex(
-        ) if isEuler == EulerianType.CIRCUIT else self.get_random_vertex_with_odd_degree()
+        ) if isEuler == EulerianType.EULER_CIRCUIT_OR_CYCLE else self.get_random_vertex_with_odd_degree()
 
         result = []
         self.dfs(startVertex, result)
         return result
-    
+
     def count_verteces(self, start):
         def count(vertex):
             visited[vertex] = True
-            count = 0
+            __count = 0
 
             for neighbour in self._graph.get_adjacent_vertices(vertex):
                 if not visited[neighbour]:
-                    count += count(neighbour)
+                    __count += count(neighbour)
 
-            return count
-        
+            return __count
+
         visited = collections.defaultdict(bool)
         return count(start)
 
@@ -200,7 +266,7 @@ class Eulerian(object):
     def can_next_edge_be_removed(self, start_vertex, neighbour):
         """
         The edge start_vertex-neighbour is valid in one of the following two cases:
-  
+
         1) If start_vertex is the only adjacent vertex of neighbour
         2) If there are multiple adjacents, then start_vertex-neighbour is not a bridge Do following steps to check if start_vertex-neighbour is a bridge
             2.a) count of vertices reachable from start_vertex
@@ -210,7 +276,7 @@ class Eulerian(object):
             2.d) If vertex count before removing is greater, then edge (start_vertex, neighbour) is a bridge
 
         """
-        
+
         if self._graph.degree(start_vertex) == 1:
             return True
         else:
@@ -223,11 +289,49 @@ class Eulerian(object):
             return False if before > after else True
 
     def dfs(self, vertex, result):
-        #Recur for all the vertices adjacent to this vertex
+        # Recur for all the vertices adjacent to this vertex
         for neighbour in self._graph.get_adjacent_vertices(vertex):
-            #If edge u-v is not removed and it's a a valid next edge
+            # If edge u-v is not removed and it's a a valid next edge
             if self.can_next_edge_be_removed(vertex, neighbour):
                 result.append((vertex, neighbour))
                 self._graph.remove_edge(vertex, neighbour)
                 self.dfs(neighbour, result)
-    
+
+
+G = Graph()
+test_data = [(1, 0), (0, 2), (2, 1), (0, 3), (3, 4), (4, 0)]
+for a, b in test_data:
+    G.add_edge(a, b)
+euler = Eulerian(G)
+print('[    (isEuler ===>, {}),     (isConnected ===>, {}),     (hasCycle ===>, {})     ]'.format(
+    euler.isEuler(), euler._cc.count() == 1, euler._cycle.has_cycle()))
+print('EULER TRAIL ===> ',euler.trail())
+
+G = Graph()
+test_data = [(1, 0), (0, 2), (2, 1), (0, 3), (3, 4)]
+for a, b in test_data:
+    G.add_edge(a, b)
+euler = Eulerian(G)
+print('[    (isEuler ===>, {}),     (isConnected ===>, {}),     (hasCycle ===>, {})     ]'.format(
+    euler.isEuler(), euler._cc.count() == 1, euler._cycle.has_cycle()))
+print('EULER TRAIL ===> ',euler.trail())
+
+G = Graph()
+test_data = [(1, 0), (0, 2), (2, 1), (0, 3), (3, 4), (1, 3)]
+for a, b in test_data:
+    G.add_edge(a, b)
+euler = Eulerian(G)
+print('[    (isEuler ===>, {}),     (isConnected ===>, {}),     (hasCycle ===>, {})     ]'.format(
+    euler.isEuler(), euler._cc.count() == 1, euler._cycle.has_cycle()))
+print('NOT A EULER GRAPH')
+G = Graph()
+test_data = [(0, 1), (1, 2), (2, 0)]
+for a, b in test_data:
+    G.add_edge(a, b)
+euler = Eulerian(G)
+print('[    (isEuler ===>, {}),     (isConnected ===>, {}),     (hasCycle ===>, {})     ]'.format(
+    euler.isEuler(), euler._cc.count() == 1, euler._cycle.has_cycle()))
+print('EULER TRAIL ===> ',euler.trail())
+
+if __name__ == '__main__':
+    doctest.testmod()
